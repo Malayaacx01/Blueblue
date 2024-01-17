819321dd90765d322af016850e718745837996c3
Malayaacx01 <111335173+Malayaacx01@users.noreply.github.com>
2024-01-17 12:57:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -138,27 +138,6 @@ ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 # // cek old script
 if [[ -r /etc/xray/domain ]]; then
 
-echo -e "${INFO} Having Script Detected !"
-echo -e "${INFO} If You Replacing Script, All Client Data On This VPS Will Be Cleanup !"
-read -p "Are You Sure Wanna Replace Script ? (Y/N) " josdong
-if [[ $josdong == "Y" ]]; then
-clear
-echo -e "${INFO} Starting Replacing Script !"
-elif [[ $josdong == "y" ]]; then
-clear
-echo -e "${INFO} Starting Replacing Script !"
-rm -rf /var/lib/scrz-prem 
-elif [[ $josdong == "N" ]]; then
-echo -e "${INFO} Action Canceled !"
-exit 1
-elif [[ $josdong == "n" ]]; then
-echo -e "${INFO} Action Canceled !"
-exit 1
-else
-echo -e "${EROR} Your Input Is Wrong !"
-exit 1
-fi
-clear
 fi
 echo -e "${GREEN}Starting Installation............${NC}"
 # // Go To Root Directory
